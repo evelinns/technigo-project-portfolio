be0d23ae8472af5d7e60f0acc2891ef3a5f2e39d
evelinns <evelinn.idenfors@gmail.com>
2025-03-28 08:44:41 +0000

    additional styling to hero component and socials

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,16 @@
 import { Socials } from './Socials';
+import { HeroImage } from './HeroImage';
 
 export const Hero = () => {
-  return <div>
+  return <div className="hero-container">
+    <div className='about-me'>
       <h2>I am Evelinn Idenfors</h2>
       <h1>Frontend Developer</h1>
       <p>Evelinn is a front-end developer with a passion for clean, functional, and visually engaging web experiences. Currently immersed in a coding bootcamp, She honing her skills in HTML, CSS, JavaScript, and modern frameworks to craft intuitive and aesthetically pleasing websites. With a keen eye for design and a love for problem-solving, she's excited to build interactive, user-friendly interfaces that make an impact. Check out her projects below—she's always learning, iterating, and creating!</p>
       <Socials />
     </div>
+    <div>
+      <HeroImage />
+    </div>
+  </div>
 }
---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,3 @@
+export const HeroImage = () => {
+  return <div className="hero-image"></div>
+}
