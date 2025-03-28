83176ace09eab01d142d7202331ee19f8be3292b
evelinns <evelinn.idenfors@gmail.com>
2025-03-28 08:20:50 +0000

    starting on hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import {Hero} from './components/Hero'
 import data from "./data.json"
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>
+  return <div>
+    <Hero />
+  </div>
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import { Socials } from './Socials';
+
+export const Hero = () => {
+  return <div>
+      <h2>I am Evelinn Idenfors</h2>
+      <h1>Frontend Developer</h1>
+      <p>Evelinn is a front-end developer with a passion for clean, functional, and visually engaging web experiences. Currently immersed in a coding bootcamp, She honing her skills in HTML, CSS, JavaScript, and modern frameworks to craft intuitive and aesthetically pleasing websites. With a keen eye for design and a love for problem-solving, she's excited to build interactive, user-friendly interfaces that make an impact. Check out her projects below—she's always learning, iterating, and creating!</p>
+      <Socials />
+    </div>
+}
